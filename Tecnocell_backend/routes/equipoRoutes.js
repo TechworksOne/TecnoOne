9ec733a3173311451b9c15ede6a2aec78a25f4bb
BYGranados04 <brennergranados@gmail.com>
2026-06-07 16:40:14 -0600

    Protege escrituras publicas de catalogos

--- Tecnocell_backend/routes/equipoRoutes.js
+++ Tecnocell_backend/routes/equipoRoutes.js
@@ -2,18 +2,21 @@
 const express = require('express');
 const router = express.Router();
 const equipoController = require('../controllers/equipoController');
+const { verifyToken, verifyRole } = require('../middleware/authMiddleware');
+
+const soloAdmin = [verifyToken, verifyRole('admin')];
 
 // Rutas para MARCAS
 router.get('/marcas', equipoController.getAllMarcas);
-router.post('/marcas', equipoController.createMarca);
-router.put('/marcas/:id', equipoController.updateMarca);
-router.delete('/marcas/:id', equipoController.deleteMarca);
+router.post('/marcas', ...soloAdmin, equipoController.createMarca);
+router.put('/marcas/:id', ...soloAdmin, equipoController.updateMarca);
+router.delete('/marcas/:id', ...soloAdmin, equipoController.deleteMarca);
 
 // Rutas para MODELOS
 router.get('/modelos', equipoController.getAllModelos);
 router.get('/marcas/:marca_id/modelos', equipoController.getModelosByMarca);
-router.post('/modelos', equipoController.createModelo);
-router.put('/modelos/:id', equipoController.updateModelo);
-router.delete('/modelos/:id', equipoController.deleteModelo);
+router.post('/modelos', ...soloAdmin, equipoController.createModelo);
+router.put('/modelos/:id', ...soloAdmin, equipoController.updateModelo);
+router.delete('/modelos/:id', ...soloAdmin, equipoController.deleteModelo);
 
 module.exports = router;
